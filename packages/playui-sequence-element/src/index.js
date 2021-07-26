8157ab1c8bdf71cec362fc4cc925812795db4399
Oxford Harrison <oxharris.me@gmail.com>
2021-07-26 07:10:06 +0100

    Renamed

---
+++ packages/playui-sequence-element/src/index.js
@@ -0,0 +1,17 @@
+
+/**
+ * @imports
+ */
+import dfn from './dfn.js';
+
+/**
+ * @definitions
+ */
+export default function(window) {
+
+	const { ul, li, div } = dfn(window);
+	
+	window.customElements.define('playui-sequence-ul', ul, {extends: 'ul'});
+	window.customElements.define('playui-sequence-li', li, {extends: 'li'});
+	window.customElements.define('playui-sequence-div', div, {extends: 'div'});
+};
