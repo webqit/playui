fc5260c6506bf3212a5b94589980d32a8b5c6a3d
Oxford <ox_harris@yahoo.com>
2023-03-17 10:39:33 +0100

    Fully assume OOHTML in context

--- packages/playui-element/src/targets.browser.js
+++ packages/playui-element/src/targets.browser.js
@@ -2,10 +2,8 @@
 /**
  * @imports
  */
-import PlayElement, { SubscriptFunction, Observer } from './PlayElement.js';
+import PlayElement from './PlayElement.js';
 
 // As globals
 if ( !self.wq ) { self.wq = {}; }
 self.wq.PlayElement = PlayElement;
-self.wq.SubscriptFunction = SubscriptFunction;
-self.wq.Observer = Observer;
